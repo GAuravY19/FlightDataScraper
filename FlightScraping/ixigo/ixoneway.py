594859e8aa20d677156c4d1e1860e1e5b01905bc
gaurav <gauravy1905@gmail.com>
2023-11-16 11:05:51 +0530

    Data Extraction for ixigo oneway trip complete. Multiway Remaining

--- FlightScraping/ixigo/ixoneway.py
+++ FlightScraping/ixigo/ixoneway.py
@@ -1,4 +1,4 @@
-
+import time
 # --------------------------------------- INFO FROM USER ---------------------------------------------------------
 
 # TRIPTYPE = input("Enter the trip type : ")
@@ -34,3 +34,64 @@ def OneWayInputs():
     return SOURCECITY, DESTINATIONCITY, DATE, MONTH, YEAR, ADULTS, CHILD, INFANTS, CLASS
 
 # ------------------------------------------------------------------------------------------------------------------
+
+
+def ScrapingIxigoOneway(DRIVER):
+    time.sleep(20)
+
+    scroll_pos_init = DRIVER.execute_script("return window.pageYOffset;")
+    stepScroll = 300
+
+    while True:
+        DRIVER.execute_script(f"window.scrollBy(0, {stepScroll});")
+        scroll_pos_end = DRIVER.execute_script("return window.pageYOffset;")
+        time.sleep(0.75)
+
+        comp = DRIVER.find_elements('xpath', '//div[@class="u-uppercase u-text-ellipsis" and text()]')
+        dpart = DRIVER.find_elements('xpath', '//div[@class="left-wing"]/div[@class="time" and text()]')
+        dboard = DRIVER.find_elements('xpath', '//div[@class="right-wing"]/div[@class="time" and text()]')
+        durtime = DRIVER.find_elements('xpath', '//div[@class="label tl " and text()]')
+        layout = DRIVER.find_elements('xpath','//div[@class="label br " and text()]')
+        price = DRIVER.find_elements('xpath', '//div[contains(@class,"c-price-display")]/span[@class="" and text()]')
+
+        if scroll_pos_init >= scroll_pos_end:
+            break
+        scroll_pos_init = scroll_pos_end
+
+
+    COMPANYNAME = []
+    DEPARTURETIME = []
+    DEBOARDTIME = []
+    DURATION = []
+    LAYOVER = []
+    PRICE = []
+
+    for i in range(len(comp)):
+        COMPANYNAME.append(comp[i].get_attribute('innerText'))
+        DEPARTURETIME.append(dpart[i].get_attribute('innerText'))
+        DEBOARDTIME.append(dboard[i].get_attribute('innerText'))
+        DURATION.append(durtime[i].get_attribute('innerText'))
+        LAYOVER.append(layout[i].get_attribute('innerText'))
+        PRICE.append(price[i].get_attribute('innerText'))
+
+
+    DATA = {
+        "Company Name":[],
+        "Departure Time":[],
+        "Deboarding Time":[],
+        "Trip Hour":[],
+        "Layover":[],
+        "Price":[]
+    }
+
+    for i in range(len(COMPANYNAME)):
+        DATA['Company Name'].append(COMPANYNAME[i])
+        DATA['Departure Time'].append(DEPARTURETIME[i])
+        DATA['Deboarding Time'].append(DEBOARDTIME[i])
+        DATA['Trip Hour'].append(DURATION[i])
+        DATA['Layover'].append(LAYOVER[i])
+        DATA['Price'].append(PRICE[i])
+
+    DATA = pd.DataFrame(DATA)
+
+    DATA.to_csv('OnewayIxigo.csv')
